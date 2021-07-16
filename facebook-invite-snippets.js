6c072592315ef2d8031c21b5885a0c42c7cd80a8
Davide <44028265+davidedexuno@users.noreply.github.com>
2021-07-17 00:03:39 +0200

    Update of facebook-invite-snippets.js

--- facebook-invite-snippets.js
+++ facebook-invite-snippets.js
@@ -1,15 +1,28 @@
 
 // Execute every snippet into Console
 
-// Invite Fans to a Page
-window.inviteDialog = document.querySelector('[role="dialog"] [role="checkbox"]').parentNode.parentNode.parentNode
-var t = setInterval(() => {
-	window.inviteDialog.scrollTo(0, window.inviteDialog.scrollHeight)
-	Array.from( document.querySelectorAll('[role="dialog"] [aria-checked="false"]') ).forEach(e => e.click())
-}, 100)
+// Common functions
+function scrollAndDo(p) {
+	window.job = setInterval(() => {
+		p.scrollBox.scrollTo(0, p.scrollBox.scrollHeight)
+		p.onScroll()
+	}, 100)
+}
+function stopScrolling() {
+	clearInterval( window.job )
+}
+
+// Invite Fans of a Page joining a group
+scrollAndDo({
+	scrollBox: document.querySelector('[role="dialog"]>:nth-child(3)>:nth-child(2)>:nth-child(1)>:nth-child(1)>:nth-child(1)>:nth-child(2)>:nth-child(1)'),
+	onScroll:  () => Array.from( document.querySelectorAll('[role="dialog"] [aria-checked="false"]') ).forEach( e => e.click() )
+})
+
+// Invite Likes to a post to like a Page
+scrollAndDo({
+	scrollBox: document.querySelector('[role="dialog"] > :nth-child(3)'),
+	onScroll:  () => Array.from( document.querySelectorAll('[role="dialog"] [role="button"]:not([aria-disabled="true"])[aria-label="Invita"]') ).forEach( e => e.click() )
+})
 
 // Stop Scrolling
-clearInterval(t)
-
-// Evaluate Memory Usage
-Math.round(window.performance.memory.totalJSHeapSize/1024/1024)
+stopScrolling()
