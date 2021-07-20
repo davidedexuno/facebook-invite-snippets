b06e814085d72136e1cb26178a6f64fa5db049f9
Davide <44028265+davidedexuno@users.noreply.github.com>
2021-07-20 16:19:55 +0200

    Update

--- facebook-invite-snippets.js
+++ facebook-invite-snippets.js
@@ -2,35 +2,37 @@
 // Execute every snippet into Console
 
 // Common functions
-function scrollAndDo(p) {
+function autoscroll(p) {
+	try {
+		var scrollBox = document.querySelector('[role="dialog"] [role="progressbar"][data-visualcompletion="loading-state"]').parentNode.parentNode.parentNode.parentNode
+	} catch(e) {
+		alert('unidentified box to scroll')
+		return
+	}
 	window.job = setInterval(() => {
-		p.scrollBox.scrollTo(0, p.scrollBox.scrollHeight)
-		p.onScroll()
-		if(p.scrollBox.offsetHeight + p.scrollBox.scrollTop < p.scrollBox.scrollHeight) {
+		if(scrollBox.offsetHeight + scrollBox.scrollTop == scrollBox.scrollHeight) {
 			return
 		}
-		if(p.scrollBox.querySelector('div:last-child [data-visualcompletion="loading-state"]')) {
+		scrollBox.scrollTo(0, scrollBox.scrollHeight)
+		if(document.querySelector('[role="dialog"] [role="progressbar"][data-visualcompletion="loading-state"]')) {
+			// keep scrolling
 			return
 		}
-		stopScrolling()
+		clearInterval( window.job )
 		setTimeout(() => alert('Scrolling complete.'), 1 * 1000)
 	}, 100)
 }
-function stopScrolling() {
+function stopAutoscroll() {
 	clearInterval( window.job )
 }
 
-// Invite Fans of a Page joining a group
-scrollAndDo({
-	scrollBox: document.querySelector('[role="dialog"]>:nth-child(3)>:nth-child(2)>:nth-child(1)>:nth-child(1)>:nth-child(1)>:nth-child(2)>:nth-child(1)'),
-	onScroll:  () => Array.from( document.querySelectorAll('[role="dialog"] [aria-checked="false"]') ).forEach( e => e.click() )
-})
+// Group
+autoscroll()
+Array.from( document.querySelectorAll('[role="dialog"] [aria-checked="false"]:not([aria-disabled="true"])') ).forEach( e => e.click() )
 
-// Invite Likes to a post to like a Page
-scrollAndDo({
-	scrollBox: document.querySelector('[role="dialog"] > :nth-child(3)'),
-	onScroll:  () => Array.from( document.querySelectorAll('[role="dialog"] [role="button"]:not([aria-disabled="true"])[aria-label="Invita"]') ).forEach( e => e.click() )
-})
+// Post
+autoscroll()
+Array.from( document.querySelectorAll('[role="dialog"] [role="button"]:not([aria-disabled="true"])[aria-label="Invita"]') ).forEach( e => e.click() )
 
 // Stop Scrolling
-stopScrolling()
+stopAutoscroll()
